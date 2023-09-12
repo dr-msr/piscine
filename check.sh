519f667fcdcb6fce8e5556a786185930cb40051a
dr-msr <me@drmsr.dev>
2023-09-13 02:57:15 +0800

    test

---
+++ check.sh
@@ -0,0 +1,17 @@
+echo "--------- CHECK, COMMPILE AND RUN" $1
+
+echo "---------- MOULINETTE"
+
+norminette -RCheckForbiddenSourceHeader "$1" | cat
+
+echo "---------- GCC"
+
+gcc -Wall -Wextra -Werror "$1" | cat
+
+echo "---------- CHMOD"
+
+chmod +x a.out
+
+echo "---------- EXECUTE"
+
+./a.out
